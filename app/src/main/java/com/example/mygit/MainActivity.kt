e608f367d69fdc75f3f06d513767563107973ea3
timurrumit0 <timur.chapliuk@gmail.com>
2024-04-14 19:13:41 +0300

    commit 1

---
+++ app/src/main/java/com/example/mygit/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mygit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        
+    }
+}
